990764d7f6679e1f5b6b3fb5a7cb7b126f238bf7
Ethoarx <moddersgithub@gmail.com>
2017-12-15 19:56:02 +0100

    Started adding examples

---
+++ examples/Klasa/events/DBANS-Detection.js
@@ -0,0 +1,39 @@
+const { Event } = require("klasa");
+const { Blacklist } = require("discordblacklist");
+const moment = require("moment");
+
+module.exports = class extends Event {
+
+    constructor(...args) {
+        super(...args, {
+            name: "guildMemberAdd",
+            enabled: true
+        });
+    }
+
+    async run(member) {
+        const banlist = new Blacklist(this.client.options.config.DBT, true, 120);
+        await banlist.update();
+        const isOnTheBanList = banlist.lookup(member.id);
+        if (!isOnTheBanList) return null;
+        const embed = new this.client.methods.Embed()
+            .setAuthor("Discord bans auto detection")
+            .setDescription(`The user ${member} has tested positive for being on the Discord bans list!`)
+            .addField(`Ban ID:`, `${isOnTheBanList.banID}`, true)
+            .addField(`Proof:`, `${isOnTheBanList.proofLink}`, true)
+            .addField(`Reason:`, `${isOnTheBanList.bannedFor}`)
+            .setColor(1)
+            .setThumbnail("https://cdn.discordapp.com/icons/269262004852621312/4947742034342a5348f6117e3e1e94bd.webp?size=1024")
+            .setFooter(`${this.client.user.username} | ${moment().format("MMMM Do YYYY, h:mm:ss a")}`, this.client.user.displayAvatarURL);
+        const channel = member.guild.channels.get(member.guild.configs.welcome);
+        if (!channel) throw "The welcome channel does not exist, did it get deleted?";
+        return channel.send({ embed });
+    }
+
+    init() {
+        if (!this.client.gateways.guilds.schema.hasKey("welcome")) {
+            return this.client.gateways.guilds.schema.addKey("welcome", { type: "Channel", default: null });
+        }
+    }
+
+};
