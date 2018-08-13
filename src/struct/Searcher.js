bae2af83886e42a3e600c1bb4da82ec8fd0db5b0
William Zhou <wzhouwzhou@gmail.com>
2018-08-13 17:06:55 -0400

    Patch https require

--- src/struct/Searcher.js
+++ src/struct/Searcher.js
@@ -1,7 +1,7 @@
 'use strict';
 Object.defineProperty(exports, '__esModule', { value: true });
 
-const http = require('https');
+const https = require('https');
 
 const default_options = {
   hostname: 'bans.discord.id',
