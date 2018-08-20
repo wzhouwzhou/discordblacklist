a95dfb60fb2d9d3cfd8da8a042e0de269cd62453
kometh0616 <realdr1v3n@gmail.com>
2018-08-20 22:25:18 +0300

    Removed an else statement.

--- src/struct/Searcher.js
+++ src/struct/Searcher.js
@@ -56,27 +56,25 @@ const Searcher = class Searcher {
     if (validIDs.length > 99) {
       Promise.reject(new Error(`More than 99 ID's for bulk lookup are not allowed!`));
     }
-    else {
-      const options = option_store.get(this);
-      let path = `?user_id=${validIDs[0]}`;
-      for (let id of validIDs) {
-        path += `&user_id=${id}`;
-      }
-      options.path += path;
-      return new Promise((res, rej) => {
-        const req = https.get(options, response => {
-          const { statusCode } = response;
-          if (statusCode !== 200) {
-            rej(new Error(`Request failed. Status Code: ${statusCode}`));
-            return response.resume();
-          }
-          let chunks = [];
-          response.on('data', chunk => chunks.push(chunk));
-          return response.on('end', () => res(JSON.parse(Buffer.concat(chunks).toString())));
-        });
-        req.end();
-      });
+    const options = option_store.get(this);
+    let path = `?user_id=${validIDs[0]}`;
+    for (let id of validIDs) {
+      path += `&user_id=${id}`;
     }
+    options.path += path;
+    return new Promise((res, rej) => {
+      const req = https.get(options, response => {
+        const { statusCode } = response;
+        if (statusCode !== 200) {
+          rej(new Error(`Request failed. Status Code: ${statusCode}`));
+          return response.resume();
+        }
+        let chunks = [];
+        response.on('data', chunk => chunks.push(chunk));
+        return response.on('end', () => res(JSON.parse(Buffer.concat(chunks).toString())));
+      });
+      req.end();
+    });
   }
 
   async isBanned(id) {
